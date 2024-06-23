9f7a6bc916e6321a63bbe9828844a8bd3c820000
Xegendvy <vystudent68@gmail.com>
2024-06-23 14:00:33 +0530

    made unit converter app

--- app/src/main/java/com/example/convertit/MainActivity.java
+++ app/src/main/java/com/example/convertit/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.convertit;
 
+import static java.lang.Integer.parseInt;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +16,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textview;
+    private EditText edittext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textview = findViewById(R.id.textView2);
+        edittext = findViewById(R.id.editTextText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Converted successfully!", Toast.LENGTH_SHORT).show();
+                String s = edittext.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.203 * kg;
+                textview.setText("The Value in Pounds is " + pound);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
